92dab387e743b25bb3ba3dc2cc7f1723feef8224
Lunna5 <git@lunna.dev>
2026-04-09 21:56:11 +0200

    feat: catppuccin theming

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,47 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import { pluginLivePreview } from './src/plugin/playground';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import { pluginLivePreview } from "./src/plugin/playground";
+import starlightCatppuccin from "@catppuccin/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://tic.lunna.dev",
-	integrations: [
-		starlight({
-			title: 'TIC',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/lunna/web-docs' }],
-			logo: {
-				src: './src/assets/logo.png',
-				replacesTitle: true
-			},
-			expressiveCode: {
-				plugins: [
-					pluginLivePreview(),
-				],
-				styleOverrides: {
-					textMarkers: {
-						insBackground: '#10b98122',
-					}
-				}
-			},
-			sidebar: [
-				{
-					label: 'Tema 1: HTML',
-					autogenerate: { directory: 'html' },
-				},
-			],
-		}),
-	],
+  site: "https://tic.lunna.dev",
+  integrations: [
+    starlight({
+      title: "TIC",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/lunna/web-docs",
+        },
+      ],
+      logo: {
+        src: "./src/assets/logo.png",
+        replacesTitle: true,
+      },
+      expressiveCode: {
+        plugins: [pluginLivePreview()],
+        styleOverrides: {
+          textMarkers: {
+            insBackground: "#10b98122",
+          },
+        },
+      },
+      sidebar: [
+        {
+          label: "Tema 1: HTML",
+          autogenerate: { directory: "html" },
+        },
+      ],
+      plugins: [
+        starlightCatppuccin({
+          dark: { flavor: "mocha"},
+          light: { flavor: "latte" },
+        }),
+      ],
+			
+    }),
+  ],
 });
